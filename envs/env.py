9ced6f9241e75857112083e46fcb75afb4125ff3
antonite <axp9304@rit.edu>
2022-12-04 18:27:41 -0500

    working multiagent env

---
+++ envs/env.py
@@ -0,0 +1,170 @@
+from copy import copy
+from gym import spaces
+import pygame
+
+import numpy as np
+from gymnasium.spaces import Discrete
+
+# from pettingzoo.utils.env import ParallelEnv
+
+
+class TestEnv():
+    metadata = {"render_modes": ["human", "rgb_array"], "render_fps": 4}
+
+    def __init__(self, render_mode=None, size=5):
+        self.size = size
+        self.window_size = 512  # The size of the PyGame window
+
+        self.foods = ["food_1"]
+        self.possible_agents = ["agent_1", "agent_2", "agent_3"]
+        # self.action_spaces = {k: spaces.Discrete(5) for k in self.possible_agents}
+        self.action_space = spaces.Discrete(5)
+
+        # every location
+        self.observation_space = {k: spaces.Discrete(3) for k in self._all_locs()}
+        self.observation_space["x"] = spaces.Discrete(size)
+        self.observation_space["y"] = spaces.Discrete(size)
+
+        self._action_to_direction = {
+            0: np.array([0, 0]),
+            1: np.array([1, 0]),
+            2: np.array([0, 1]),
+            3: np.array([-1, 0]),
+            4: np.array([0, -1]),
+        }
+        assert render_mode is None or render_mode in self.metadata["render_modes"]
+        self.render_mode = render_mode
+        self.window = None
+        self.clock = None
+
+    def _all_locs(self):
+        # every location
+        locKeys = []
+        for i in range(self.size):
+            for j in range(self.size):
+                locKeys.append(str(i)+","+str(j))
+        return locKeys
+
+    def _get_obs(self):
+        # every location
+        grid = {k: 0 for k in self._all_locs()}
+        # check agents
+        for v in self.agent_locations.values():
+            grid[str(v[0])+","+str(v[1])] = 1
+        # check foods
+        for v in self.food_locations.values():
+            grid[str(v[0])+","+str(v[1])] = 2
+        observations = {k: grid for k in self.agent_locations}
+        # position agents
+        for k in self.agent_locations:
+            observations[k]["x"] = self.agent_locations[k][0]
+            observations[k]["y"] = self.agent_locations[k][1]
+        return observations
+
+    def reset(self):
+        self.agents = copy(self.possible_agents)
+        self.agent_locations = {k: [0, 0] for k in self.agents}
+        self.food_locations = {k: [2, 2] for k in self.foods}
+        # if self.render_mode == "human":
+        #     self._render_frame()
+
+        return self._get_obs()
+
+    def isOnFood(self, aloc):
+        for f in self.food_locations:
+            if np.array_equal(aloc, self.food_locations[f]):
+                return True
+        return False
+
+    def step(self, actions, render):
+        directions = {k: self._action_to_direction[actions[k]] for k in actions}
+        self.agent_locations = {k: np.clip(
+            self.agent_locations[k] + directions[k], 0, self.size - 1) for k in directions}
+        reaches = {k: self.isOnFood(self.agent_locations[k]) for k in directions}
+        rewards = {k: 1 if reaches[k] else 0 for k in reaches}
+
+        if self.render_mode == "human" and render:
+            self._render_frame()
+
+        return self._get_obs(), rewards
+
+    def render(self):
+        if self.render_mode == "rgb_array":
+            return self._render_frame()
+
+    def _render_frame(self):
+        if self.window is None and self.render_mode == "human":
+            pygame.init()
+            pygame.display.init()
+            self.window = pygame.display.set_mode((self.window_size, self.window_size))
+        if self.clock is None and self.render_mode == "human":
+            self.clock = pygame.time.Clock()
+
+        canvas = pygame.Surface((self.window_size, self.window_size))
+        canvas.fill((255, 255, 255))
+        pix_square_size = (
+            self.window_size / self.size
+        )  # The size of a single grid square in pixels
+
+        # First we draw the foods
+        for f in self.food_locations.values():
+            loc = [p * pix_square_size for p in f]
+            pygame.draw.rect(
+                canvas,
+                (0, 255, 0),
+                pygame.Rect(
+                    loc,
+                    (pix_square_size, pix_square_size),
+                ),
+            )
+
+        # Now we draw the agents
+        color = 255
+        i = 0
+        for a in self.agent_locations.values():
+            colorb = 255 - 80*i
+            colorr = 80*i
+            i += 1
+            loc = [(p + 0.5) * pix_square_size for p in a]
+            pygame.draw.circle(
+                canvas,
+                (colorr, 0, colorb),
+                loc,
+                pix_square_size / 3,
+            )
+
+        # Finally, add some gridlines
+        for x in range(self.size + 1):
+            pygame.draw.line(
+                canvas,
+                0,
+                (0, pix_square_size * x),
+                (self.window_size, pix_square_size * x),
+                width=3,
+            )
+            pygame.draw.line(
+                canvas,
+                0,
+                (pix_square_size * x, 0),
+                (pix_square_size * x, self.window_size),
+                width=3,
+            )
+
+        if self.render_mode == "human":
+            # The following line copies our drawings from `canvas` to the visible window
+            self.window.blit(canvas, canvas.get_rect())
+            pygame.event.pump()
+            pygame.display.update()
+
+            # We need to ensure that human-rendering occurs at the predefined framerate.
+            # The following line will automatically add a delay to keep the framerate stable.
+            self.clock.tick(self.metadata["render_fps"])
+        else:  # rgb_array
+            return np.transpose(
+                np.array(pygame.surfarray.pixels3d(canvas)), axes=(1, 0, 2)
+            )
+
+    def close(self):
+        if self.window is not None:
+            pygame.display.quit()
+            pygame.quit()
---
+++ main.py
@@ -0,0 +1,117 @@
+import torch
+from torch.autograd import Variable
+import random
+import gym
+import time
+
+from envs.env import TestEnv
+env = TestEnv(render_mode="human")
+
+
+# Number of states
+n_state = len(env.observation_space)
+# Number of actions
+n_action = env.action_space.n
+# Number of hidden nodes in the DQN
+n_hidden = 32
+# Learning rate
+lr = 0.001
+
+print(n_state)
+print(n_action)
+
+
+class Agent():
+    ''' Deep Q Neural Network Agent. '''
+
+    def __init__(self, state_dim, action_dim, hidden_dim=32, lr=0.05):
+        self.criterion = torch.nn.MSELoss()
+        self.model = torch.nn.Sequential(
+            torch.nn.Linear(state_dim, hidden_dim),
+            torch.nn.LeakyReLU(),
+            torch.nn.Linear(hidden_dim, hidden_dim*2),
+            torch.nn.LeakyReLU(),
+            torch.nn.Linear(hidden_dim*2, action_dim)
+        )
+        self.optimizer = torch.optim.Adam(self.model.parameters(), lr)
+
+    def update(self, state, y):
+        """Update the weights of the network given a training sample. """
+        y_pred = self.model(torch.Tensor(list(state.values())))
+        loss = self.criterion(y_pred, Variable(torch.Tensor(y)))
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+    def predict(self, state):
+        """ Compute Q values for all actions using the DQL. """
+        with torch.no_grad():
+            return self.model(torch.Tensor(list(state.values())))
+
+
+def q_learning(env, gamma=0.9, epsilon=0.3, eps_decay=0.999):
+    """Deep Q Learning algorithm using the DQN. """
+    state = env.reset()
+    agents = {}
+    for k in state:
+        agents[k] = Agent(n_state, n_action, n_hidden, lr)
+
+    steps = 30000
+    for i in range(steps):
+        actions = {}
+        for a in agents:
+            # Implement greedy search policy to explore the state space
+            if random.random() < epsilon:
+                action = env.action_space.sample()
+            else:
+                q_values = agents[a].predict(state[a])
+                action = torch.argmax(q_values).item()
+
+            actions[a] = action
+
+        # Take action and add reward to total
+        render = i > 5000
+        obs, rewards = env.step(actions, render)
+
+        # every expected weighted action for every agent
+        q_values_l = {k: agents[k].predict(state[k]).tolist() for k in obs}
+
+        if i % 100 == 0:
+            print("step: " + str(i))
+            for a in actions:
+                print("agent: " + str(a) + ", action: " + str(actions[a]) + ", reward: " + str(rewards[a]))
+                print("qvals: " + str(q_values_l[a]))
+
+        # if done:
+        #     q_values[action] = reward
+        #     # Update network weights
+        #     agents[agent].update(state, q_values)
+        #     deadAgents.append(agents[agent])
+        #     del agents[agent]
+        #     continue
+
+        # Update network weights using the last step only
+        q_values_next = {k: agents[k].predict(obs[k]) for k in obs}
+        for a in q_values_l:
+            q_values_l[a][action] = rewards[a] + gamma * torch.max(q_values_next[a]).item()
+            agents[a].update(state[a], q_values_l[a])
+
+        state = obs
+
+        # Update epsilon
+        epsilon = max(epsilon * eps_decay, 0.01)
+        # time.sleep(0.1)
+
+
+q_learning(env, gamma=.5, epsilon=0.3)
+input()
+
+
+# for agent in env.agent_iter():
+#     observation, reward, termination, truncation, info = env.last()
+#     action = None if termination or truncation else env.action_space(
+#         agent).sample()
+#     env.step(action)
+#     print(agent)
+#     print(action)
+# simple_dqn = Agent(n_state, n_action, n_hidden, lr)
